8f144f920820b3422f1f07dec8ad9bc44e6fc175
Charles Spitzer <charles.spitzer@dont-nod.com>
2022-12-05 12:31:48 +0100

    Patch after review

--- 04/Program.cs
+++ 04/Program.cs
@@ -7,5 +7,5 @@ Console.WriteLine(
     input.Select(line =>
         Regex.Split(line, @"(\d+)-(\d+),(\d+)-(\d+)")
         .Where(x => x != "").Select(x => int.Parse(x)).ToArray())
-    .Count(v => Math.Abs(Math.Sign(v[0]-v[2]) + Math.Sign(v[1]-v[3])) < 2)
+    .Count(v => (v[0]-v[2]) * (v[1]-v[3]) <= 0)
 );
