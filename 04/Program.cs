0a56524fc1415dba21bda20fb32ce23312ce4fe2
Charles Spitzer <charles.spitzer@dont-nod.com>
2022-12-06 15:00:54 +0100

    Added day 6

--- 04/Program.cs
+++ 04/Program.cs
@@ -5,8 +5,5 @@ var input = File.ReadAllLines("04/input.txt")
             .Select(line => Regex.Split(line, @"(\d+)-(\d+),(\d+)-(\d+)")
             .Where(x => x != "").Select(x => int.Parse(x)).ToArray());
 
-// Part 1:
 Console.WriteLine(input.Count(v => (v[0]-v[2]) * (v[1]-v[3]) <= 0));
-
-// Part 2:
 Console.WriteLine(input.Count(v => (v[0]-v[3]) * (v[1]-v[2]) <= 0));
---
+++ 06/Program.cs
@@ -0,0 +1,7 @@
+﻿string input = File.OpenText("06/input.txt").ReadLine();
+
+int detect(string s, int len)
+    => s.Select((x,i) => i).Skip(len-1).TakeWhile(i => s.Substring(i-len+1,len).Distinct().Count() < len).Count() + len;
+
+Console.WriteLine(detect(input, 4));
+Console.WriteLine(detect(input, 14));
